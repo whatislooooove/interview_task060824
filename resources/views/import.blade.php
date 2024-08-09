c42082972c57cb34bbe2098789f264fcbeb8cb3e
whatislove <sadovin.vladislav@gmail.com>
2024-08-09 16:11:38 +0300

    Frontend on import page done

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -1,26 +1,14 @@
 @extends('layouts.app')
 @section('content')
-
-            <main class="mt-6">
-                <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
-                    <form method="post" action="{{route('import.upload')}}" enctype="multipart/form-data">
-                        @csrf
-                        <div class="form-group">
-                            <label for="file">Выберите файл</label>
-                            <input type="file" name="file" id="file" class="form-control">
-                            <button type="submit" class="btn btn-success grid gap-6 lg:grid-cols-1 lg:gap-8 flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">Import</button>
-                        </div>
-                    </form>
-
-                    <a
-                        href="{{route('product.index')}}"
-                        class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
-                    >
-
-                        <div class="pt-3 sm:pt-5">
-                            <h2 class="text-xl font-semibold text-black dark:text-white">Products</h2>
-                        </div>
-                    </a>
-                </div>
-            </main>
+    <div class="container form-group my-4">
+        <form method="post" action="{{route('import.upload')}}" enctype="multipart/form-data">
+            @csrf
+            <div class="mb-3">
+                <label for="file" class="form-label">Choose a file</label>
+                <input type="file" class="form-control" id="fileInput" aria-describedby="fileHelp" required accept="application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet">
+                <div id="fileHelp" class="form-text">Upload excel-file for import data. File must have .xls or .xlsx extension</div>
+            </div>
+            <button type="submit" class="btn btn-primary">Import</button>
+        </form>
+    </div>
 @endsection
